edc53f724c73b6c45a705e7b7ae590b129f457ba
A1ecm <37123158+A1ecm@users.noreply.github.com>
2018-12-08 21:22:23 +0100

    Add files via upload

---
+++ ChessGame/chess/public/javascripts/move.js
@@ -0,0 +1,17 @@
+var Chess = require('../../node_modules/chess.js/chess').Chess;
+var chess = new Chess();
+
+alert("game.js started.");
+
+window.onload = function () {
+    document.getElementById("a2").addEventListener("click", clicked);
+    function clicked(){
+      alert("a2 clicked");
+      var movespiece = chess.moves({square: 'a2'});
+      console.log(movespiece);
+      for(let i = 0; i < movespiece.length; i++){
+          movespiece[i].style.color = "green";
+      }
+    }
+
+  }
