7dd6be427b9aeaeaf61a13a89c5109ecf5344786
Thomas-Markhorst <Thomas-Markhorst@users.noreply.github.com>
2018-12-09 11:59:37 +0100

    d

--- ChessGame/chess/public/javascripts/coordinates.js
+++ ChessGame/chess/public/javascripts/coordinates.js
@@ -1,7 +1,18 @@
-var clicked = false;
+var Chess = require('../../node_modules/chess.js/chess').Chess;
+// var readline = require('readline-sync');
+var chess = new Chess();
 
-function showMoves(el){
-    alert(el.id);
-
+function main(){
+    "use strict";
+    var pieceSelect = false;
+    $(".chessboard td").on("click", function(event){
+        if(event.target.innerHTML != ""){
+            pieceSelect = true;
+            alert(chess.moves({square: event.target.id}));
+        }
+       
+    
+    });
 
 };
+$(document).ready(main);
--- ChessGame/chess/public/javascripts/move.js
+++ ChessGame/chess/public/javascripts/move.js
@@ -1,16 +1,16 @@
-// var Chess = require('../../node_modules/chess.js/chess').Chess;
-var chess = new Chess();
+// // var Chess = require('../../node_modules/chess.js/chess').Chess;
+// var chess = new Chess();
 
 
-window.onload = function () {
-    document.getElementById("a2").addEventListener("click", clicked);
-    function clicked(){
-      alert("a2 clicked");
-      var movespiece = chess.moves({square: 'a2'});
-      console.log(movespiece);
-      for(let i = 0; i < movespiece.length; i++){
-          movespiece[i].style.color = "green";
-      }
-    }
+// window.onload = function () {
+//     document.getElementById("a2").addEventListener("click", clicked);
+//     function clicked(){
+//       alert("a2 clicked");
+//       var movespiece = chess.moves({square: 'a2'});
+//       console.log(movespiece);
+//       for(let i = 0; i < movespiece.length; i++){
+//           movespiece[i].style.color = "green";
+//       }
+//     }
 
-  }
+//   }
